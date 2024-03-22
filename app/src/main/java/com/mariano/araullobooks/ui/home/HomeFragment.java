3d25ae643a62060e08f2a1496e6d9f59163d0be9
Lorenzo-Mariano <hhumminah@gmail.com>
2024-03-22 12:14:55 +0800

    Home page now checks for user log in state

--- app/src/main/java/com/mariano/araullobooks/ui/home/HomeFragment.java
+++ app/src/main/java/com/mariano/araullobooks/ui/home/HomeFragment.java
@@ -4,28 +4,38 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import com.mariano.araullobooks.Globals;
+import com.mariano.araullobooks.R;
 import com.mariano.araullobooks.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
-
     private FragmentHomeBinding binding;
+    private Globals globals;
+    private boolean isLoggedIn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        globals = Globals.getInstance();
+        isLoggedIn = globals.isLoggedIn();
+
+        if (isLoggedIn) {
+            View loggedInView = inflater.inflate(R.layout.fragment_home_logged_in, container, false);
+            binding.getRoot().addView(loggedInView);
+        } else {
+            View loggedOutView = inflater.inflate(R.layout.fragment_home_logged_out, container, false);
+            binding.getRoot().addView(loggedOutView);
+        }
+
         return root;
     }
 
@@ -35,3 +45,6 @@ public class HomeFragment extends Fragment {
         binding = null;
     }
 }
+
+
+
